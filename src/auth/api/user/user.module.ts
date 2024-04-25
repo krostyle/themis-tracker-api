9bab6d54845b9b5ac5355e7f0c58a3eb222ed490
krostyle <diegoo.albarran@gmail.com>
2024-04-24 22:58:24 -0400

    fix: fix dependencies

--- src/auth/api/user/user.module.ts
+++ src/auth/api/user/user.module.ts
@@ -1,14 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { RegisterUserUseCase } from "@/auth/application/use-cases/register-user.use-case";
-import { UserDbRepository } from "@/auth/infrastructure/database/user/repositories/user.repository";
 
 @Module({
   imports: [],
-  providers: [
-    RegisterUserUseCase,
-    { provide: "UserRepository", useClass: UserDbRepository },
-  ],
+  providers: [RegisterUserUseCase],
   controllers: [UserController],
 })
 export class UserModule {}
